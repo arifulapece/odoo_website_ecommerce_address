e3a5048d9769f1354dde99f7fae5167dc0a6a48c
Md Ariful Islam <arifulapece@gmail.com>
2023-06-14 15:16:46 +0300

    models

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Website Ecommerce Address",
+    "summary": "Set e-Commerce Checkout Address by District and Area",
+    "version": "1.0",
+    "category": "Website",
+    "website": "https://superglobalhost.com",
+    "author": "Super Global Host",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["website_sale"],
+    "assets": {
+        "web.assets_tests": [
+            "/website_ecommerce_address/static/src/js/website_ecommerce_address_tour.js"
+        ]
+    },
+}
